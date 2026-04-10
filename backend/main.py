12595608b17a2af35d81109d2bfa4d95f1d6ca27
Tomoe Tezuka <tomoe0905@mailbox.unideb.hu>
2026-04-10 20:12:33 +0200

    feat: create FastAPI endpoint for audio file uploads

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/analyze/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
